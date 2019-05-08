07f4c1ed657cde9eb87f736be68a1af6cdf8d07f
Corwin Harris <git@corwin.betabeetle.com>
2019-05-08 11:45:36 -0400

    Removed `SCALE` since color lerping allows us to use arbitrary sizes.

--- src/main.h
+++ src/main.h
@@ -11,24 +11,20 @@
 
 /* Simulation constants */
 
-// SCALE: This value set the default scale of the window. Since HEIGHT is externally dependent it's easier to draw at
-// the above HEIGHT * WIDTH resolution and just scale up the entire texture. (Default is 4)
-const static size_t SCALE = 3;
-
 // HEIGHT: The value was chosen due to how the original algo works. CLASSIC_PALETTE_SIZE (defined in DoomFire.h) affects how
 // high the flames are drawn. Thus we need a value that isn't too short or too tall. The original value, 168 looks
 // really good with some but not too much vertical space to allow for little `sparks`. However, since the resolution is
 // dependent on the CLASSIC_PALETTE_SIZE in the first place, I'm adopting a calculated calue in order to support additional
 // resolutions down the road.
-constexpr static size_t HEIGHT = SCALE * DoomFire::CLASSIC_PALETTE_SIZE;
+constexpr static size_t HEIGHT = 240;
 
 // WIDTH: The value uses the above to choose a width that results in a 16:9 ratio which is the current most common
 // screen size.
-const static size_t WIDTH = floor(HEIGHT * (16.0 / 9.0));
+constexpr static size_t WIDTH = (size_t) (HEIGHT * (16.0 / 9.0));
 
 // TARGET_TICK_RATE: This is our target FPS (more or less) down the road I may split ticks from frames and run the sim
 // at either a higher or lower tick rate, but keep FPS locked to some common value, 30 or 60).
-const static uint TARGET_TICK_RATE = 28;
+const static uint TARGET_TICK_RATE = 70;
 
 // SECOND_NS: The number of nanoseconds in a second.
 const static uint SECOND_NS = 1000000;
