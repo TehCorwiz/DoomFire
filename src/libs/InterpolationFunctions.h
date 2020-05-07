446ff42c5b41848513efcf1cedc0ac44987c9f24
Corwin Harris <git@corwin.betabeetle.com>
2020-05-07 13:12:18 -0400

    Variable name change for consistency.

--- src/libs/InterpolationFunctions.h
+++ src/libs/InterpolationFunctions.h
@@ -11,11 +11,11 @@ size_t interpolateLinear(const double v1, const double v2, const double mu) {
     return ceil(v1 * (1 - mu) + v2 * mu);
 }
 
-size_t interpolateCosine(const double y1, const double y2, const double mu) {
+size_t interpolateCosine(const double v1, const double v2, const double mu) {
     double mu2;
 
     mu2 = (1 - cos(mu * M_PI)) / 2;
-    return ceil(y1 * (1 - mu2) + y2 * mu2);
+    return ceil(v1 * (1 - mu2) + v2 * mu2);
 }
 
 #endif //DOOMFIRE_INTERPOLATIONFUNCTIONS_H
